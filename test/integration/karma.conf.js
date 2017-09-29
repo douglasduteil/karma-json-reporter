fdd649c804309f0bcb8645b4e814d0ede3d00749
Douglas Duteil <douglasduteil@gmail.com>
2017-09-29 20:12:08 +0200

    chore(travis): force single run

--- test/integration/karma.conf.js
+++ test/integration/karma.conf.js
@@ -55,7 +55,7 @@ module.exports = function (config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: process.env.KARMA_SINGLE_RUN || false,
 
     // Concurrency level
     // how many browser should be started simultaneous
